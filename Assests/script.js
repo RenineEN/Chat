38102140f6247942c4653284e270e80b46d238bb
RenineEN <156373333+RenineEN@users.noreply.github.com>
2024-09-23 08:42:31 -0500

    Create script.js

---
+++ Assests/script.js
@@ -0,0 +1,7 @@
+document.getElementById("skyward").onclick = function(){
+  window.open("https://login.microsoftonline.com/c6bfee2d-24f8-41d7-88a1-214cad6717da/saml2");
+}
+
+document.getElementById("canvas").onclick = function(){
+  window.open("https://garlandisd.instructure.com/login/saml");
+}
