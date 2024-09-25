9d960e43e94d73067581463b5704c0affffe4d6c
RenineEN <156373333+RenineEN@users.noreply.github.com>
2024-09-25 07:56:06 -0500

    Update script.js

--- Assests/script.js
+++ Assests/script.js
@@ -2,18 +2,18 @@ window.addEventListener("load", (event) => {
     console.log("page is fully loaded");
 });
 
-document.getElementById("skyward").onclick = function(){
+document.getElementById("canvas").onclick = function(){
     try{
-        window.open("https://skyward10.iscorp.com/SkySTSgarlandtxstu/sso/Skyward/login/SSO", type="_blank");
+        window.open("https://garlandisd.instructure.com/login/saml");
     } catch (err){
         console.log(err);
         alert(err);
     }
 }
 
-document.getElementById("canvas").onclick = function(){
+document.getElementById("skyward").onclick = function(){
     try{
-        window.open("https://garlandisd.instructure.com/login/saml");
+        window.open("https://skyward10.iscorp.com/SkySTSgarlandtxstu/sso/Skyward/login/SSO");
     } catch (err){
         console.log(err);
         alert(err);
